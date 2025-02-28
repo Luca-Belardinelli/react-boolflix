8c715e62ecd4b9ecea3ccf8eee938ed2f9eef442
Luca Belardinelli <belardinelliluca@gmail.com>
2025-02-28 12:40:00 +0100

    default flag

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,13 +1,17 @@
 export default function FilmCard(props) {
 
+    //DESTRUTTURO L'OGGETTO PROPS
     const { film } = props;
+
+    //VARIABILE GESTIONE BANDIERE
+    const flag = "/flag/Flag_of_Italy.svg"
 
     return (
         <div className="container">
             <div className="filmcard" key={film.id}>
                 <h1>Titolo {film.title}</h1>
                 <h2> Titolo originale {film.original_title} </h2>
-                <p> Lingua {film.original_language}</p>
+                <img src={`/flag/${film.original_language}.png`} alt="{film.original_language}" onError={(e) => e.target.src = flag} />
                 <p> Voto {film.vote_average}</p>
             </div>
         </div>
