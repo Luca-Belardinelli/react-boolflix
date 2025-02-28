9471f755ab49f6c7a80eb71691522b21730bbbdc
Luca Belardinelli <belardinelliluca@gmail.com>
2025-02-28 18:11:33 +0100

    FontAwesomeIcon + star

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,3 +1,8 @@
+// FontAwesomeIcon 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+
 export default function FilmCard({ film, serie }) {
 
     //DESTRUTTURO L'OGGETTO PROPS
@@ -5,6 +10,38 @@ export default function FilmCard({ film, serie }) {
 
     //VARIABILE GESTIONE BANDIERE
     const flag = "/flag/jp.png"
+
+    //FUNIONE STAR
+    const stars = (vote) => {
+
+        const pointer = Math.ceil(vote / 2);
+
+        let stars = [];
+
+        for (let i = 0; i < 5; i++) {
+
+            if (i < pointer) {
+                stars.push(
+                    <FontAwesomeIcon
+                        key={i}
+                        icon={faStar}
+                        style={{ color: "gold" }}
+                    />
+                );
+
+            } else {
+                stars.push(
+                    <FontAwesomeIcon
+                        key={i}
+                        icon={faStar}
+                        style={{ color: "#e6e6e6" }}
+                    />
+                );
+
+            }
+        }
+        return stars;
+    };
 
 
     return (
@@ -20,7 +57,7 @@ export default function FilmCard({ film, serie }) {
                     <img className="imgf" src={`https://image.tmdb.org/t/p/w342${film.poster_path}`} alt={film.title} />
                     <h4> Titolo originale: {film.original_title} </h4>
                     <img className="flag" src={`/flag/${film.original_language}.png`} alt="{film.original_language}" onError={(e) => e.target.src = flag} />
-                    <p> Voto {film.vote_average}</p>
+                    <p> Voto: {stars(film.vote_average)}</p>
                 </div>
             )}
 
@@ -30,11 +67,11 @@ export default function FilmCard({ film, serie }) {
 
                 <div className="card" key={serie.id}>
                     <h1>Sezione SERIE</h1>
-                    <h1>Titolo {serie.title}</h1>
+                    <h1>Titolo: {serie.title}</h1>
                     <img className="imgf" src={`https://image.tmdb.org/t/p/w342/${serie.poster_path}`} alt={serie.name} />
-                    <h2> Titolo originale {serie.original_name} </h2>
+                    <h2> Titolo originale: {serie.original_name} </h2>
                     <img className="flag" src={`/flag/${serie.original_language}.png`} alt="{serie.original_language}" onError={(e) => e.target.src = flag} />
-                    <p> Voto {serie.vote_average}</p>
+                    <p> Voto: {stars(serie.vote_average)}</p>
                 </div>
             )}
         </>
